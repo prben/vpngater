4a2a5eb95e5b9bf5f24d588cd028838c8119f4ff
bipul.pramanick <bipul.pramanick@flipkart.com>
2020-08-21 13:35:36 +0530

    run builds in docker

--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-os="$(uname -s)"
-
 test -d builds || mkdir builds
 for i in linux darwin; do
-env GOOS=$i GOARCH=amd64 go build -o builds/vpngater-$i .
+	docker run --rm -d -v $PWD:/go/src/vpngater -w /go/src/vpngater golang:1.14-buster env GOOS=$i GOARCH=amd64 go build -o builds/vpngater-$i
 done
-
